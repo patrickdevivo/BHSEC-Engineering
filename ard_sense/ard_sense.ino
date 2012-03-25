64f513ea791ecc2f6eb3b8523cb416921fc5f0df
Zaid Qureshi <zaidhorse@gmail.com>
2012-03-24 20:08:44 -0400

    so this is basically my code. -it read all the sensors and then it makes an array, or a list -if a sensor is activated it puts the number of the sensor in the array -the maximum number of sensor that can be put in the array, or be "active" at a time is 2 -then it adds the two numbers together and spits it to serial. so: 1.the naming convention for the sound files need to be 0-11 for the 12 notes per 1 sensor and then the number from the name of the two notes added 2. we will be using digital pins 0-11 on the arduino right?
    Signed-off-by: Zaid Qureshi <zaidhorse@gmail.com>

---
+++ ard_sense/ard_sense.ino
@@ -0,0 +1,45 @@
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(0, INPUT);
+  pinMode(1, INPUT); 
+  pinMode(2, INPUT); 
+  pinMode(3, INPUT); 
+  pinMode(4, INPUT); 
+  pinMode(5, INPUT); 
+  pinMode(6, INPUT); 
+  pinMode(7, INPUT); 
+  pinMode(8, INPUT); 
+  pinMode(9, INPUT); 
+  pinMode(10, INPUT); 
+  pinMode(11, INPUT); 
+
+
+
+} 
+
+void loop()
+{
+  int sense_status[2];
+  int count = 0;
+  for(int n=0; n<12; n++)
+  {
+    if (digitalRead(n)==1)
+    {
+      if (n==1) 
+      {
+        sense_status[0]=n;
+      }
+      else 
+      {
+        sense_status[1]=n;
+      }
+    }
+  } 
+  Serial.println(sense_status[0]+sense_status[1]);
+
+
+}
+
