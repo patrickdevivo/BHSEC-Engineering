58a63201c7527a3a32a61cc0aa8b1a8a52552ef8
ZaidQureshi <zaidhorse@gmail.com>
2012-04-27 23:30:33 -0300

    Update Sensor Arduino/ard_sense/ard_sense.ino

--- Sensor Arduino/ard_sense/ard_sense.ino
+++ Sensor Arduino/ard_sense/ard_sense.ino
@@ -1,31 +1,18 @@
-//Setup
 void setup()
 {
-  //Start Serial
   Serial.begin(9600);
-  //Establish sensor-pins 2-13
-  for(int p=2; p<14; p++)
+  for(int i=2; i<=13; i++)
   {
-    pinMode(p, INPUT);
+    pintMode(i, INPUT);
   }
-  
-} 
-//Start Loop
+}
+
 void loop()
 {
-  //Make a new empty string of 12 characters
-  char sense_status[12];
-  //Start a counter
-  int count = 0;
-  //For loop for reading sensors
-  for(int n=2; n<14; n++)
+  for(int p=2; p<=13; p++)
   {
-    //Reading sensors and putting value in the string in the place corresponding to sensor #
-    sense_status[count]=digitalRead(n);
-    count+=1;
-  } 
-  //Print string in serial prompt to communicate with other Arduino
-  Serial.print(sense_status);
+    Serial.print(digitalRead(p));
+    digitalWrite(p, LOW);
+  }
   Serial.println(".WAV!");
-
 }
