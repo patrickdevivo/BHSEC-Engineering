8c31f5ee4b68819273a5f1a905dc620769c78a0f
ZaidQureshi <zaidhorse@gmail.com>
2012-05-05 04:06:49 -0300

    this is with updated naming scheme

--- Wave Arduino/ard_play/ard_play.ino
+++ Wave Arduino/ard_play/ard_play.ino
@@ -12,14 +12,7 @@ FatReader f;      // This holds the information for the file we're play
 
 WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
 
-#define DEBOUNCE 5  // button debouncer
-
-// here is where we define the buttons that we'll use. button "1" is the first, button "6" is the 6th, etc
-byte buttons[] = {14, 15, 16, 17, 18, 19};
-// This handy macro lets us determine how big the array up above is, by checking the size
-#define NUMBUTTONS sizeof(buttons)
-// we will track if a button is just pressed, just released, or 'pressed' (the current state
-volatile byte pressed[NUMBUTTONS], justpressed[NUMBUTTONS], justreleased[NUMBUTTONS];
+#define DEBOUNCE 100  // button debouncer
 
 // this handy function will return the number of bytes currently free in RAM, great for debugging!   
 int freeRam(void)
@@ -47,15 +40,11 @@ void sdErrorCheck(void)
 }
 
 void setup() {
-  byte i;
-  
   // set up serial port
   Serial.begin(9600);
-  putstring_nl("WaveHC with ");
-  Serial.print(NUMBUTTONS, DEC);
-  putstring_nl("buttons");
+  putstring_nl("WaveHC with 6 buttons");
   
-  putstring("Free RAM: ");       // This can help with debugging, running out of RAM is bad
+   putstring("Free RAM: ");       // This can help with debugging, running out of RAM is bad
   Serial.println(freeRam());      // if this is under 150 bytes it may spell trouble!
   
   // Set the output pins for the DAC control. This pins are defined in the library
@@ -67,12 +56,14 @@ void setup() {
   // pin13 LED
   pinMode(13, OUTPUT);
  
-  // Make input & enable pull-up resistors on switch pins
-  for (i=0; i< NUMBUTTONS; i++) {
-    pinMode(buttons[i], INPUT);
-    digitalWrite(buttons[i], HIGH);
-  }
-  
+  // enable pull-up resistors on switch pins (analog inputs)
+  digitalWrite(14, HIGH);
+  digitalWrite(15, HIGH);
+  digitalWrite(16, HIGH);
+  digitalWrite(17, HIGH);
+  digitalWrite(18, HIGH);
+  digitalWrite(19, HIGH);
+ 
   //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
   if (!card.init()) {         //play with 8 MHz spi (default faster!)  
     putstring_nl("Card init. failed!");  // Something went wrong, lets print out why
@@ -109,73 +100,20 @@ void setup() {
   
   // Whew! We got past the tough parts.
   putstring_nl("Ready!");
-  
-  TCCR2A = 0;
-  TCCR2B = 1<<CS22 | 1<<CS21 | 1<<CS20;
-
-  //Timer2 Overflow Interrupt Enable
-  TIMSK2 |= 1<<TOIE2;
-
-
 }
-
-SIGNAL(TIMER2_OVF_vect) {
-  check_switches();
-}
-
-void check_switches()
-{
-  static byte previousstate[NUMBUTTONS];
-  static byte currentstate[NUMBUTTONS];
-  byte index;
-
-  for (index = 0; index < NUMBUTTONS; index++) {
-    currentstate[index] = digitalRead(buttons[index]);   // read the button
-    
-    /*     
-    Serial.print(index, DEC);
-    Serial.print(": cstate=");
-    Serial.print(currentstate[index], DEC);
-    Serial.print(", pstate=");
-    Serial.print(previousstate[index], DEC);
-    Serial.print(", press=");
-    */
-    
-    if (currentstate[index] == previousstate[index]) {
-      if ((pressed[index] == LOW) && (currentstate[index] == LOW)) {
-          // just pressed
-          justpressed[index] = 1;
-      }
-      else if ((pressed[index] == HIGH) && (currentstate[index] == HIGH)) {
-          // just released
-          justreleased[index] = 1;
-      }
-      pressed[index] = !currentstate[index];  // remember, digital HIGH means NOT pressed
-    }
-    //Serial.println(pressed[index], DEC);
-    previousstate[index] = currentstate[index];   // keep a running tally of the buttons
-  }
-}
-
 
 void loop() {
-  byte i;
-  playfile();
-  
-}
-
-void playfile() {
-  char message[16];
+  char message[12];
  
   while (Serial.available() <2)
   {
   }
-  for(int j = 0; j < 17; j++)
+  for(int j = 0; j < 13; j++)
   {
     message[j] = '\0';
   }
   char ch = Serial.read();
-  for(int i = 0; i < 16 && ch != '!' ; i++, ch = Serial.read()) 
+  for(int i = 0; i < 13 && ch != '!' ; i++, ch = Serial.read()) 
   { 
  
     message[i] = ch; 
@@ -183,14 +121,30 @@ void playfile() {
     {
     }
   }
+  playfile(message);
+}
 
+
+
+
+// Plays a full file from beginning to end with no pause.
+void playcomplete(char *name) {
+  // call our helper to find and play this name
+  playfile(name);
+  while (wave.isplaying) {
+  // do nothing while its playing
+  }
+  // now its done playing
+}
+
+void playfile(char *name) {
   // see if the wave object is currently doing something
   if (wave.isplaying) {// already playing something, so stop it!
     wave.stop(); // stop it
   }
   // look in the root directory and open the file
-  if (!f.open(root, message)) {
-    putstring("Couldn't open file "); Serial.print(message); return;
+  if (!f.open(root, name)) {
+    putstring("Couldn't open file "); Serial.print(name); return;
   }
   // OK read the file and turn it into a wave object
   if (!wave.create(f)) {
